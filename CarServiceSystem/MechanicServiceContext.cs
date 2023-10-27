754ca8c341fd1bd0a599270e7a75173e76663bf8
JH-Tran <jhtran023@gmail.com>
2023-10-27 16:47:13 +1100

    Fixed Typo in MechanicServiceContext

--- CarServiceSystem/MechanicServiceContext.cs
+++ CarServiceSystem/MechanicServiceContext.cs
@@ -5,13 +5,16 @@ namespace CarServiceSystem
     public class MechanicServiceContext : DbContext
     {
         public DbSet<Customer> Customers { get; set; }
-
+        public DbSet<ServiceAdmin> ServiceAdmins { get; set; }
+        public DbSet<Mechanic> Mechanics { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<ServiceLog> ServiceLogs { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqlocaldb;Database=MechanicServiceDb;Trusted_Connection=True");
-
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=MechanicServiceDb;Trusted_Connection=True");
         }
     }
 }
